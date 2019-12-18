620f8ca2b3b41b7328a2f6085a90be9be6152982
Gorazd Kmet <gorazdkmet@MacAir.local>
2019-12-18 16:36:13 +0100

    added jumping and menu actions

---
+++ src/js/data.js
@@ -0,0 +1,64 @@
+
+export const data = {
+	sk: {
+		fieldNav: {
+			paints: 'maľba',
+			sound: 'zvuk',
+			video: 'video',
+			more: 'viac',
+			lan: 'en'
+		},
+		paint: {
+			transfig: {
+				name: 'Transfigurácie',
+				intro: '',
+				fotos: [
+					'Transformácia III., 145 x 160 cm, akryl, olej a syntetická prírodnina na plátne, 2019',
+					'Transformácia IV., 23 x 25 cm, kombinovaná technika na plátne, 2019',
+					'Transformácia I., 130 x 160 cm, akryl a syntetická prírodnina na plátne, 2018',
+					'Do vnútra, 70 x 90 cm, akryl a syntetická prírodnina na plátne, 2019',
+					'Ulita, 140 x 170 cm, akryl a syntetická prírodnina na plátne, 2018'
+				]
+			},
+			transform: {
+				name: 'Transformácie',
+				intro: '',
+				fotos: [
+					'Obrázok bez názvu 0',
+					'Obrázok bez názvu 1',
+					'Obrázok bez názvu 2',
+					'Obrázok bez názvu 3',
+					'Obrázok bez názvu 4'
+				]
+			},
+			allocation: 'Vymedzenie',
+			deposits: 'Sedimenty',
+			oecesis: 'Zakorenenie',
+			denatura: 'Denatura'
+		},
+		sound: {
+			sound1: 'Zvuk 1',
+			sound2: 'Zvuk 2',
+			sound3: 'Zvuk 3',
+			sound4: 'Zvuk 4',
+			sound5: 'Zvuk 5'
+		},
+		video: {
+			video1: 'Video 1',
+			video2: 'Video 2',
+			video3: 'Video 3',
+			video4: 'Video 4',
+			video5: 'Video 5',
+		},
+		more: {
+			more1: 'Viac 1',
+			more2: 'Viac 2',
+			more3: 'Viac 3',
+			more4: 'Viac 4',
+			more5: 'Viac 5',
+		}
+	},
+	en: {
+
+	}
+};
